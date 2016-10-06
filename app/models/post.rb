6aeacdbf69e2238819306b75bbfcdd4cbf72ca6d
tiennv91 <tiennv.study@gmail.com>
2016-10-06 16:22:17 +0700

    validate presence of tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ActiveRecord::Base
 	acts_as_taggable # Alias for acts_as_taggable_on :tags
 
   validates :title, presence: true
+  validates_presence_of :tag_list
 
   has_attached_file :cover_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.jpg"
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\z/
